dc6b6fd36e2507d2bc409121d4dbd4f170e3f096
Villeveikko Sula <villeveikkosula@hotmail.com>
2016-01-13 22:55:12 +0200

    Koitetaas
    Perushommeleita Host - kansioon

--- src/host/Silo.java
+++ src/host/Silo.java
@@ -1,5 +1,39 @@
 package host;
 
 public class Silo extends Thread{
-
+  public boolean varattu;
+  public String tyyppi;
+  public int täyttöaste; // kiloa, max. 10000 kg
+  
+  public Silo() {
+    täyttöaste = 0;
+    varattu = false;
+  }
+  
+  public boolean getVarattu() {
+    return varattu;
+  }
+  public void setVarattu(boolean varattu) {
+    this.varattu = varattu;
+  }
+  
+  public String getTyyppi() {
+    return tyyppi;
+  }
+  public void setTyyppi(String tyyppi) {
+    this.tyyppi = tyyppi;
+  }
+  
+  public int getTäyttöaste() {
+    return täyttöaste;
+  }
+  public boolean setTäyttöaste(int täyttöaste) {
+    if (this.täyttöaste + täyttöaste <= 10000) {
+      this.täyttöaste = täyttöaste;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
